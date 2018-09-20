ad95f92220c2a1c67c0d5783a487a0fdbcf723d2
Issaret Prachitmutita <imac.monochrome@gmail.com>
2018-09-20 16:45:35 +0700

    For setup docker file

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo true
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get install apt-transport-https
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce=18.03.1~ce-0~ubuntu 
+sudo usermod -aG docker ${USER}
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
