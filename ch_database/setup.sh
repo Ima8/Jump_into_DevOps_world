2cbfb75413ba90dd52cfd4910885ef618b4d3495
Issaret Prachitmutita <imac.monochrome@gmail.com>
2018-09-22 22:39:25 +0700

    Change database back to mysql galera

--- ch_database/setup.sh
+++
@@ -1,6 +0,0 @@
-sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8
-sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.3/ubuntu xenial main'
-sudo apt-get update
-sudo apt-get install mariadb-server -y
-sudo apt-get install rsync -y
-mysql -furoot < "mysql_secure_installation.sql"
---
+++ ch_database/setup_first.sh
@@ -0,0 +1,17 @@
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv BC19DDBA
+sudo add-apt-repository 'deb http://releases.galeracluster.com/mysql-wsrep-5.6/ubuntu xenial main'
+sudo add-apt-repository 'deb http://releases.galeracluster.com/galera-3/ubuntu xenial main'
+sudo echo "Package: *" >> /etc/apt/preferences.d/galera.pref
+sudo echo "Pin: origin releases.galeracluster.com" >> /etc/apt/preferences.d/galera.pref
+sudo echo "Pin-Priority: 1001" >> /etc/apt/preferences.d/galera.pref
+sudo apt-get update
+sudo apt-get install galera-3 galera-arbitrator-3 mysql-wsrep-5.6 -y
+sudo apt-get install rsync -y
+mysql -furoot < "mysql_secure_installation.sql"
+sudo systemctl stop mysql
+sudo cp galera.cnf /etc/mysql/conf.d/galera.cnf
+sudo /etc/init.d/mysql start --wsrep-new-cluster
+
+## sudo 
+
+
